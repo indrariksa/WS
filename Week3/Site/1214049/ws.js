cc934b1350444d9b1f61061f5c9dd8de43cda666
Aulianafhrn <aulianafahrian@gmail.com>
2023-03-03 15:16:32 +0700

    3-2B-1214049-Auliana Fahrian Bani Ridwan

---
+++ Week3/Site/1214049/ws.js
@@ -0,0 +1,39 @@
+const { get } = require("http");
+
+function PostSignUp(namadepan,namabelakang,email,password){
+  var myHeaders = new Headers();
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "namadepan": namadepan,
+  "namabelakang": namabelakang,
+  "email": email,
+  "password": password
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://eotyaryigz3hsqb.m.pipedream.net", requestOptions)
+  .then(response => response.text())
+  .then(result => GetResponse(result))
+  .catch(error => console.log('error', error));
+}
+
+function GetResponse(result){
+  document.getElementById("formsignup").innerHTML = result;
+}
+
+function PushButton(){
+  namadepan=document.getElementById("namadepan").value;
+  namabelakang=document.getElementById("namabelakang").value;
+  email=document.getElementById("email").value;
+  password=document.getElementById("password").value;
+  PostSignUp(namadepan,namabelakang,email,password);
+}
